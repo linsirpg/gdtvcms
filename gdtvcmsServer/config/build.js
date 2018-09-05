4975b5a8e38da91c9ae5fcf394c9d2950f4f6c6a
linsirpg <766459364@qq.com>
2018-09-05 21:57:52 +0800

    gdtvcms 数据管理系统

--- gdtvcmsServer/config/build.js
+++ gdtvcmsServer/config/build.js
@@ -6,45 +6,6 @@ module.exports = {
     // port: 8001,
     port: 3000,
     keys: '1',
-    // database: {
-    //     gdtvShop: {
-    //         database: 'gdtvshop',
-    //         username: 'gdtvroot',
-    //         password: 'mHnMTwlfYen6ygb2',
-    //         options: {
-    //             host: 'gdtvshopr1.mysql.rds.aliyuncs.com',
-    //         }
-    //     },
-    //     balance: {
-    //         database: 'gdtv_balance',
-    //         username: 'gdtvbalance',
-    //         password: 'kvIxoEkKVmCeURn6',
-    //         options: {
-    //             host: 'gdtvcmsdb.mysql.rds.aliyuncs.com',
-    //         }
-    //     },
-    // }
     database: {
-        //  cms:{
-        //      database: 'gdtv_cms',
-        //     username: 'root',
-        //     password: '123456',
-        //     options: {
-        //         define: {  charset: 'utf8' },
-        //         // host: '192.168.0.101',
-        //         host: '127.0.0.1',
-        //         dialect: 'mysql',
-        //     }
-        // },
-        // cms:{
-        //      database: 'gdtv_cms',
-        //     username: 'gdtv_cms',
-        //     password: 'zlan2017',
-        //     options: {
-        //         host: '114.215.183.168',
-        //         define: {  charset: 'utf8' },
-        //         dialect: 'mysql',
-        //     }
-        // }      
     }
 };
